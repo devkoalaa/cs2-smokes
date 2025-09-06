b771f164b9f01a1533f0bb7da7431f1885eb4111
devkoalaa <herberticleber@gmail.com>
2025-09-06 17:48:57 -0300

    update config

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,7 @@
 export const config = {
-  apiUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:6969',
+  apiUrl: process.env.NEXT_PUBLIC_API_URL || 'https://cs2-smokes-hub-api.vercel.app',
   steam: {
-    returnUrl: process.env.NEXT_PUBLIC_STEAM_RETURN_URL || 'http://localhost:5757/auth/callback',
-    realm: process.env.NEXT_PUBLIC_STEAM_REALM || 'http://localhost:5757',
+    returnUrl: process.env.NEXT_PUBLIC_STEAM_RETURN_URL || 'https://cs2smokeshub.vercel.app/auth/callback',
+    realm: process.env.NEXT_PUBLIC_STEAM_REALM || 'https://cs2smokeshub.vercel.app',
   },
 } as const;
